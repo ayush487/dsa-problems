fd6445eb16bcee8e8b9e831628e3c5037c50ce37
ayush487 <ak487457@gmail.com>
2026-09-21 00:26:54 +0530

    Solved Neetcode Problems : Maximum Depth of Binary Tree

---
+++ neetcode/Problem088.java
@@ -0,0 +1,30 @@
+//Maximum Depth of Binary Tree
+
+public class Problem088 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int maxDepth(TreeNode root) {
+        return depth(root, 0);
+    }
+
+    private int depth(TreeNode node, int currentDepth) {
+        if (node==null) return currentDepth;
+        return Math.max(depth(node.left, currentDepth+1), depth(node.right, currentDepth+1));
+    }
+}
