2633cf9b441f8a62f8e98a9b21ea3558d11dd689
ayush487 <ak487457@gmail.com>
2026-09-21 03:40:33 +0530

    Solved Neetcode Problems : Subtree of Another Tree

---
+++ neetcode/Problem092.java
@@ -0,0 +1,47 @@
+//Subtree of Another Tree
+
+public class Problem092 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    boolean isSubTree = false;
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        isSub(root, subRoot);
+        return isSubTree;
+    }
+
+    private void isSub(TreeNode root, TreeNode subRoot) {
+        if (isSameTree(root, subRoot)) {
+            isSubTree = true;
+            return;
+        }
+        if (root.left != null)
+            isSub(root.left, subRoot);
+        if (root.right != null)
+            isSub(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if ((p == null && q != null) || (p != null && q == null)) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
