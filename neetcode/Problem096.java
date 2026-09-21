c3598fb91a4c14d4d604e045c16e4dc28551a810
ayush487 <ak487457@gmail.com>
2026-09-21 17:00:44 +0530

    Solved Neetcode Problems : Binary Tree Level Order Traversal

---
+++ neetcode/Problem096.java
@@ -0,0 +1,57 @@
+//Binary Tree Level Order Traversal
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Problem096 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static class TreeNodeLevels {
+        int level;
+        TreeNode node;
+
+        TreeNodeLevels(int level, TreeNode node) {
+            this.level = level;
+            this.node = node;
+        }
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<TreeNodeLevels> queue = new LinkedList<>();
+        if (root != null) queue.offer(new TreeNodeLevels(0, root));
+        while (!queue.isEmpty()) {
+            TreeNodeLevels lNode = queue.poll();
+            int alSize = list.size();
+            if (alSize == lNode.level) {
+                list.add(new ArrayList<>());
+            }
+            list.get(lNode.level).add(lNode.node.val);
+            if (lNode.node.left != null)
+                queue.offer(new TreeNodeLevels(lNode.level + 1, lNode.node.left));
+            if (lNode.node.right != null)
+                queue.offer(new TreeNodeLevels(lNode.level + 1, lNode.node.right));
+        }
+        return list;
+    }
+}
