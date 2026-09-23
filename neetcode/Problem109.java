ba20ee4e5e35030036e1c2a8dc9af6cc31062b28
ayush487 <ak487457@gmail.com>
2026-09-23 21:22:42 +0530

    Solved Neetcode Problems : Kth Largest Element in an Array

---
+++ neetcode/Problem109.java
@@ -0,0 +1,14 @@
+//Kth Largest Element in an Array
+
+import java.util.PriorityQueue;
+
+public class Problem109 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.poll();
+    }
+}
