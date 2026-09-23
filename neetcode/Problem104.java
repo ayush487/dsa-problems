97f69a0ddd2924149ed2c355db207c44bdcdaa14
ayush487 <ak487457@gmail.com>
2026-09-23 14:57:11 +0530

    Solved Neetcode Problems : House Robber III

---
+++ neetcode/Problem104.java
@@ -0,0 +1,51 @@
+//House Robber III
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem104 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private Map<TreeNode, Integer> cache;
+
+    public int rob(TreeNode root) {
+        cache = new HashMap<>();
+        cache.put(null, 0);
+        int ans = findMaxRobbery(root);
+        return ans;
+    }
+
+    private int findMaxRobbery(TreeNode root) {
+        if (root == null) return 0;
+        if (cache.containsKey(root)) return cache.get(root);
+
+        int res = root.val;
+
+        if (root.left != null)
+            res += findMaxRobbery(root.left.left) + findMaxRobbery(root.left.right);
+        if (root.right != null)
+            res += findMaxRobbery(root.right.left) + findMaxRobbery(root.right.right);
+
+        res = Math.max(res, findMaxRobbery(root.left) + findMaxRobbery(root.right));
+        cache.put(root, res);
+        return res;
+
+    }
+}
